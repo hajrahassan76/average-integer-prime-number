848a0b84fdda399bd1b05823f281131db97caafd
hajrahassan76 <85940529+hajrahassan76@users.noreply.github.com>
2021-06-22 16:44:44 +0500

    Create average.py

---
+++ average.py
@@ -0,0 +1,10 @@
+list=[90,80,90,40,50,60]
+sum_list=0
+for number in list:
+    sum_list+=number
+avg_list=sum_list/len(list)
+print('avg=',avg_list)
+#or
+    #sum_list=number+list
+#ag_list=sum_list/len(list)
+#print('the average is',ag_list)
